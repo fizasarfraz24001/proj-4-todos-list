849eef58222e37563ddea0276e3687abc16b3ea5
fizasarfraz24001 <fizasarfraz24001@gmail.com>
2024-05-31 23:11:22 +0500

    to-do list

---
+++ main.ts
@@ -0,0 +1,24 @@
+import inquirer from 'inquirer'
+let todos = [];
+let condition = "true";
+while(condition)
+
+{let todosQuestion = await inquirer.prompt(
+    [
+        {
+            name: "Question1",
+            type: "input",
+            message:"What would you like to add in your todos",
+        },
+        {
+            name:"Question2",
+            type:"confirm",
+            message:"Anything else you need to put  in your todos",
+            default:"false",
+        }
+    ]
+);
+         todos.push(todosQuestion.Question1);
+         console.log(todos)
+         condition = todosQuestion.Question2;
+}
